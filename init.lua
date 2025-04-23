24578c06ce0406f19aa53c27fff60decb3354d7e
Obsydian-Falcon <walk.dauphin@gmail.com>
2025-04-22 22:18:18 -0600

    Tab autocompletion activated

--- init.lua
+++ init.lua
@@ -753,14 +753,14 @@ require('lazy').setup({
         end)(),
         dependencies = {
           -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          --  See the README about individual language/framework/plugin snippets:
+          --  https://github.com/rafamadriz/friendly-snippets
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
       },
       'saadparwaiz1/cmp_luasnip',
@@ -790,19 +790,46 @@ require('lazy').setup({
         --
         -- No, but seriously. Please read `:help ins-completion`, it is really good!
         mapping = cmp.mapping.preset.insert {
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.confirm { select = true }
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+
+          -- j: move down (next item)
+          ['j'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end, { 'i', 'c' }),
+
+          -- k: move up (previous item)
+          ['k'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()
+            end
+          end, { 'i', 'c' }),
+
+          ['<CR'] = cmp.mapping.confirm { select = true },
           -- Select the [n]ext item
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          -- Select the [p]revious item
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-
-          -- Scroll the documentation window [b]ack / [f]orward
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-          -- Accept ([y]es) the completion.
-          --  This will auto-import if your LSP supports it.
-          --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          --           ['<C-n>'] = cmp.mapping.select_next_item(),
+          --           -- Select the [p]revious item
+          --           ['<C-p>'] = cmp.mapping.select_prev_item(),
+          --
+          --           -- Scroll the documentation window [b]ack / [f]orward
+          --           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          --           ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          --
+          --           -- Accept ([y]es) the completion.
+          --           --  This will auto-import if your LSP supports it.
+          --           --  This will expand snippets if the LSP sent a snippet.
+          --           ['<C-y>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
